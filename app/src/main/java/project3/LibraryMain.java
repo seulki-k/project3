8097b40e5ec3dde6356d67d2695395e891a9fca6
seulk <skbb11@naver.com>
2024-07-11 14:36:28 +0900

    a

--- app/src/main/java/project3/LibraryMain.java
+++ app/src/main/java/project3/LibraryMain.java
@@ -1,16 +1,32 @@
 package project3;
 
+import project3.command.LibraryCommand;
+import project3.util.Prompt;
+import project3.vo.AnsiCode;
+
+import java.util.Scanner;
+
 public class LibraryMain {
 
-    public static String[] mainMenus = {"등록", "조회","삭제","종료"};
+    public static String[] mainMenus = {"등록", "조회", "대출", "삭제", "종료"};
+    static LibraryCommand libraryCommand = new LibraryCommand();
+
 
     public static void main(String[] args) {
         LibraryMain.printMainMenus();
+        Scanner scanner = new Scanner(System.in);
+        int command = Prompt.inputInt("입력할 번호는?", (Object) mainMenus);
+        System.out.println(command);
+        libraryCommand.excute();
     }
 
-    public static void printMainMenus(){
+    public static void printMainMenus() {
+        System.out.println("\n" + AnsiCode.ANSI_YELLOW + "           [도서 관리 시스템]     " + AnsiCode.ANSI_RESET);
+        int count = 1;
         for (String mainMenu : mainMenus) {
-            System.out.println(mainMenu);
+            System.out.println(count + ". " + mainMenu);
+            count++;
         }
+        System.out.println();
     }
 }
--- app/src/main/java/project3/vo/Book.java
+++ app/src/main/java/project3/vo/Book.java
@@ -1,16 +1,21 @@
 package project3.vo;
 
+import java.time.LocalDate;
+
 public class Book {
     int name; // 책의 이름
 
-    boolean available; // 책 대출 가능 여부
+    boolean available; // 책 대출 가능 여부, true 대여가능, false 대여 중.
 
     String content; // 책의 간략한 내용
 
-    public Book(int name, boolean available, String content) {
+    LocalDate date; //반납 예정일 혹은 대여 예정일. available 값에 따라 용도 변경
+
+    public Book(int name, boolean available, String content, LocalDate date) {
         this.name = name;
         this.available = available;
         this.content = content;
+        this.date = date;
     }
 
     public int getName() {
