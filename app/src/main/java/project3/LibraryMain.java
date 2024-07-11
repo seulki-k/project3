3646c1494dc53089eda05aee0658413618a19579
seulk <skbb11@naver.com>
2024-07-11 19:59:39 +0900

    a

--- app/src/main/java/project3/LibraryMain.java
+++ app/src/main/java/project3/LibraryMain.java
@@ -4,61 +4,41 @@ import project3.command.*;
 import project3.vo.AnsiCode;
 
 import java.util.HashMap;
-import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
 
 public class LibraryMain {
-    Map<String, Command> commandMap = new HashMap<>();
+    static Map<String, Command> commandMap = new HashMap<>();
     public static String[] menus = {"등록", "조회", "대여", "반납", "수정", "삭제", "종료"};
 
-    public static AddBookCommand addBookCommand = new AddBookCommand();
-    public static ViewBookCommand viewBookCommand = new ViewBookCommand();
-    public static BorrowBookCommand borrowBookCommand = new BorrowBookCommand();
-    public static ReturnBookCommand returnBookCommand = new ReturnBookCommand();
-    public static UpdateBookCommand updateBookCommand = new UpdateBookCommand();
-    public static DeleteBookCommand deleteBookCommand = new DeleteBookCommand();
     public static TestCommand testCommand = new TestCommand();
 
+    public LibraryMain() {
+        commandMap.put("등록", new AddBookCommand());
+        commandMap.put("조회", new ViewBookCommand());
+        commandMap.put("대여", new BorrowBookCommand());
+        commandMap.put("반납", new ReturnBookCommand());
+        commandMap.put("수정", new UpdateBookCommand());
+        commandMap.put("삭제", new DeleteBookCommand());
+    }
+
     public static void main(String[] args) {
-
-
+        new LibraryMain();
         LibraryMain.printMainMenus();
         testCommand.execute();
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("[Main] > ");
             try {
                 String command = scanner.nextLine();
-                if (command.equals("menu")) {
-                    printMainMenus();
-                }
-                int commandParse = Integer.parseInt(command);
-                switch (commandParse) {
-                    case 1:
-                        addBookCommand.execute();
-                        break;
-                    case 2:
-                        viewBookCommand.execute();
-                        break;
-                    case 3:
-                        borrowBookCommand.execute();
-                        break;
-                    case 4:
-                        returnBookCommand.execute();
-                        break;
-                    case 5:
-                        updateBookCommand.execute();
-                        break;
-                    case 6:
-                        deleteBookCommand.execute();
-                        break;
-                    case 7:
-                        System.out.println("도서 관리 시스템을 종료합니다.");
-                        return;
-                }
-            } catch (NumberFormatException e) {
+                if (command.equals("menu")) printMainMenus(); //menu 입력 시 메뉴 출력
+                if (command.equals("7")) return; // 7입력 시 종료
+                Command com = commandMap.get(menus[Integer.parseInt(command) - 1]);
+                com.execute();
+
+            } catch (Exception e) {
                 System.out.println("잘못된 입력입니다.");
             }
         }
