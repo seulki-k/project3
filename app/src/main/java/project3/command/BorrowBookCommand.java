24a6fc163f9ff9e454d7c9d0752b8f4d18a882d5
seulk <skbb11@naver.com>
2024-07-12 14:00:06 +0900

    a

--- app/src/main/java/project3/command/BorrowBookCommand.java
+++ app/src/main/java/project3/command/BorrowBookCommand.java
@@ -1,11 +1,12 @@
 package project3.command;
 
-// 1) 보관(대여 가능 (도서 리스트 출력)
+// 1) 보관(대여 가능) 도서 리스트 출력
+// 보관 도서 리스트는 날짜가 출력될 필요가 없습니다.
 // 2) 도서 대여
 // 3) 대여 기간 추가.
 
-// 인덱스 번호 입력하여 available 값 true에서 false로 변경
-//
+// 인덱스 번호 입력하여 available 값 true(보관)에서 false(대여)로 변경
+
 public class BorrowBookCommand implements Command{
 
 
