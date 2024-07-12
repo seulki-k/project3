c23e29f0652ce00148312ca1f5d77e4443a43a38
seulk <skbb11@naver.com>
2024-07-12 09:47:24 +0900

    a

--- app/src/main/java/project3/command/DeleteBookCommand.java
+++ app/src/main/java/project3/command/DeleteBookCommand.java
@@ -12,16 +12,18 @@ public class DeleteBookCommand implements Command{
         ArrayList<Book> book = AddBookCommand.book;
         Scanner scanner = new Scanner(System.in);
 
-        ViewBookCommand view = new ViewBookCommand();
-
         while (true) {
-            view.printAll(book);
-
+            int size = book.size();
             System.out.print("제거할 도서의 제목 (이전 0): ");
             String deleteName = scanner.nextLine(); //제거할 도서의 제목을 전달 받음
             if(deleteName.equals("0"))return;
             // 람다형식 사용 book의 객체 book1 생성 후 해당 객체의 이름과 입력받은 이름이 동일하면 삭제
             book.removeIf(book1 -> book1.getName().equals(deleteName));
+            if (size == book.size()){
+                System.out.println("해당 도서가 없습니다.");
+            }else {
+                System.out.println(deleteName + " 도서가 삭제되었습니다.");
+            }
         }
     }
 }
